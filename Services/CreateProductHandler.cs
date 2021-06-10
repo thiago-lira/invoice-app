5dfce11307ca1da6be1d94b8fadb59041e283d8f
Thiago Lira <lira_thiago@msn.com>
2021-06-10 13:58:41 -0300

    create product

---
+++ Services/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using Core.Features;
+using Core.Models;
+using Infrastructure;
+
+namespace Services
+{
+    public class CreateProductHandler
+    {
+        private readonly IProductRepository _repository;
+
+        public CreateProductHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(CreateProduct createProduct)
+        {
+            var product = new Product()
+            {
+                Name = createProduct.Name,
+                Price = createProduct.Price
+            };
+
+            _repository.Save(product);
+        }
+    }
+}
