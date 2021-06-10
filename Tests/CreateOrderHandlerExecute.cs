67f5f96eb4cddf614fbc002ba610d945fb97aede
Thiago Lira <lira_thiago@msn.com>
2021-06-10 16:48:03 -0300

    add an important TODO

---
+++ Tests/CreateOrderHandlerExecute.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class CreateOrderHandlerExecute
+    {
+        [Fact]
+        public void GivenACreateOrderCommandThenSaveIt()
+        {
+        }
+    }
+}
