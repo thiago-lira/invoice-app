e2f97484f8fc51add343544a647f9620260d9ca2
Thiago Lira <lira_thiago@msn.com>
2021-06-12 20:46:22 -0300

    create base repository

---
+++ Infrastructure/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Core.Models;
+
+namespace Infrastructure
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public Product GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
