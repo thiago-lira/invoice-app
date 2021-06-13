eb160acacf71a10ed56fe779a5a55af3ccd883d6
Thiago Lira <lira_thiago@msn.com>
2021-06-13 20:40:03 -0300

    create endpoint for seller creation

---
+++ API/Controllers/SellersController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Core.Features;
+using Core.Models;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class SellersController : ControllerBase
+    {
+        private readonly ISellerRepository _repository;
+
+        public SellersController(ISellerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Seller seller)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var handler = new CreateSellerHandler(_repository);
+                var command = new CreateSeller(seller.Name, seller.Email, seller.Address);
+
+                await handler.Execute(command);
+
+                return NoContent();
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
