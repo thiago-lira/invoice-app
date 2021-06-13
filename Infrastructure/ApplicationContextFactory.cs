afad0273111e408ca2b7cda0b159425adb567c69
Thiago Lira <lira_thiago@msn.com>
2021-06-12 21:15:14 -0300

    add migrations

---
+++ Infrastructure/ApplicationContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlite("Data Source=temp.db");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
